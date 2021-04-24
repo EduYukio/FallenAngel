3eee60447b63b2e2a24cc000c4ab7ff7f79630a4
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-04-24 11:33:50 -0300

    Add Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    private void OnCollisionEnter2D(Collision2D other) {
+        GameObject otherObj = other.gameObject;
+        if (otherObj.CompareTag("Player")) {
+            Player player = otherObj.GetComponent<Player>();
+            if (player.isInvulnerable) return;
+
+            if (player.hp > 0) player.TakeDamage();
+        }
+    }
+}
