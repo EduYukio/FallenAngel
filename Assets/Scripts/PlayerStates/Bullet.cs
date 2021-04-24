ec292a278af7ac13855137b93d97c140188e9a15
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-04-24 18:29:59 -0300

    Adapt bullet to new enemies

--- Assets/Scripts/PlayerStates/Bullet.cs
+++ Assets/Scripts/PlayerStates/Bullet.cs
@@ -4,8 +4,16 @@ public class Bullet : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
         GameObject otherObj = other.gameObject;
         if (otherObj.CompareTag("Enemy")) {
-            Enemy enemy = otherObj.GetComponent<Enemy>();
-            if (enemy.hp > 0) enemy.TakeDamage();
+            Groundy groundy = otherObj.GetComponent<Groundy>();
+            Fly fly = otherObj.GetComponent<Fly>();
+
+            if (groundy != null) {
+                if (groundy.hp > 0) groundy.TakeDamage();
+            }
+            if (fly != null) {
+                if (fly.hp > 0) fly.TakeDamage();
+            }
+
             Destroy(gameObject);
         }
         else if (otherObj.CompareTag("Ground")) {
