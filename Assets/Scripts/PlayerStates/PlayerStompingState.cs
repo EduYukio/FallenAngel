536c99117e437a8b85c883e6ad609008e4c73f1a
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-04-25 17:44:10 -0300

    Fix bug where the stomp would make the player jump higher

--- Assets/Scripts/PlayerStates/PlayerStompingState.cs
+++ Assets/Scripts/PlayerStates/PlayerStompingState.cs
@@ -7,17 +7,32 @@ public class PlayerStompingState : PlayerBaseState {
     }
 
     public override void Update(Player player) {
+        // if (player.rb.velocity.y > player.stompForce) player.rb.velocity = new Vector2(player.rb.velocity.x, player.stompForce);
         base.ProcessMovementInput(player);
 
         if (base.CheckTransitionToShooting(player)) return;
         if (base.CheckTransitionToGrounded(player)) return;
-        if (base.CheckTransitionToFalling(player)) return;
+        if (CheckTransitionToFalling(player)) return;
     }
 
     void Setup(Player player) {
+        player.isGrounded = false;
     }
 
     void JumpAction(Player player) {
         player.rb.velocity = new Vector2(player.rb.velocity.x, player.stompForce);
+    }
+
+    public override bool CheckTransitionToFalling(Player player) {
+        if (player.isGrounded) return false;
+
+        bool playerIsFalling = player.rb.velocity.y <= 0;
+
+        if (playerIsFalling) {
+            player.TransitionToState(player.FallingState);
+            return true;
+        }
+
+        return false;
     }
 }
