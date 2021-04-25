74204b046354e4b4473eb1f51e2525713c205125
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-04-25 07:44:16 -0300

    Add bullet range/lifetime

--- Assets/Scripts/PlayerStates/Bullet.cs
+++ Assets/Scripts/PlayerStates/Bullet.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
+    public float lifetime = 2f;
+    private void Start() {
+        Destroy(gameObject, lifetime);
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         GameObject otherObj = other.gameObject;
         if (otherObj.CompareTag("Groundy")) {
