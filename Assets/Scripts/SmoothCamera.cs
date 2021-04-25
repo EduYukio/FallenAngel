f6ad5ac9ba63b1eb76f0a3d2494b51d901219d66
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-04-25 06:11:33 -0300

    Add smooth camera movement

---
+++ Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour {
+    Player player;
+    public float moveSpeed = 0.5f;
+
+    private void Start() {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update() {
+        Vector2 nextPosition = new Vector2(player.transform.position.x, player.transform.position.y - 2f);
+        Vector2 camPos = new Vector2(transform.position.x, transform.position.y);
+        float distance = (camPos - nextPosition).magnitude;
+        if (Mathf.Abs(distance) < 0.1f) return;
+
+        Vector2 posCoords = Vector2.Lerp(transform.position, nextPosition, Time.deltaTime * moveSpeed);
+        transform.position = new Vector3(posCoords.x, posCoords.y, -10f);
+    }
+}
